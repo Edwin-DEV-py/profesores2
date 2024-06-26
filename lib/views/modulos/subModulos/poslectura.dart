5e5f75ebc08827fb6c57a2353325019a8d0a28f6
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-06-26 14:16:01 -0500

    postlectura

--- lib/views/modulos/subModulos/poslectura.dart
+++ lib/views/modulos/subModulos/poslectura.dart
@@ -1,4 +1,16 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:profesores2/components/botones.dart';
+import 'package:profesores2/components/items.dart';
+import 'package:profesores2/components/titulos.dart';
+import 'package:profesores2/components/urls.dart';
+import 'package:profesores2/views/modulos/subModulos/submodulosPostlectura/post1.dart';
+import 'package:profesores2/views/modulos/subModulos/submodulosPostlectura/post2.dart';
+import 'package:profesores2/views/modulos/subModulos/submodulosPostlectura/post3.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class ModuloPostlectura extends StatefulWidget {
   const ModuloPostlectura({super.key});
@@ -10,6 +22,40 @@ class ModuloPostlectura extends StatefulWidget {
 class _ModuloPostlecturaState extends State<ModuloPostlectura> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            titulos2Modulos('POSTLECTURA'),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    video_show('zI_KFPWJzhI'),
+                    SizedBox(height: 20,),
+                    itemWithImage2(context, 30,'POSTLECTURA 1', 'assets/libro2.png', 0.2, () => Get.to(() => Post1(),transition: Transition.downToUp, duration: Duration(milliseconds: 300))),
+                    SizedBox(height: 10,),
+                    itemWithImage2(context, 30,'POSTLECTURA 2', 'assets/libro2.png', 0.2, () => Get.to(() => Post2(),transition: Transition.downToUp, duration: Duration(milliseconds: 300))),
+                    SizedBox(height: 10,),
+                    itemWithImage2(context, 30,'POSTLECTURA 3', 'assets/libro2.png', 0.2, () => Get.to(() => Post3(),transition: Transition.downToUp, duration: Duration(milliseconds: 300))),
+                    SizedBox(height: 20,),
+                    btnAzul1('FINALIZAR',
+                    () async{
+                      Get.back();
+                      SharedPreferences prefs = await SharedPreferences.getInstance();
+                      await prefs.setBool('modulo_conclusiones_activo', true);
+                    }
+                    ),
+                    SizedBox(height: 20,),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
